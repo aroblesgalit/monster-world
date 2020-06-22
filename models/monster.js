2cd315917ef407d99ecc500d878cc02e29d69e54
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-06-22 02:45:34 -0500

    Added a bunch of extra stats to monster

--- models/monster.js
+++ models/monster.js
@@ -3,7 +3,30 @@ const Schema = mongoose.Schema;
 
 const monsterSchema = new Schema({
     name: { type: String, required: true },
+
     level: { type: Number, required: true },
+    exp: { type: Number, required: true },
+    // Will we need this one here?
+    nextLevel: { type: Number, required: true },
+
+    size:{ type: Number },
+    weight:{ type: Number },
+
+    stat: {
+        happiness:{ type: Number },
+        hunger:{ type: Number },
+
+        strength:{ type: Number },
+        defense:{ type:Number},
+        speed:{ type: Number },
+        int:{ type: Number },
+
+        ice:{ type: Number },
+        fire:{ type: Number },
+        earth:{ type: Number },
+        air:{ type: Number }
+        // decide on full magic/types chart
+    },
     moves: [
         {
             title: { type: Number },
