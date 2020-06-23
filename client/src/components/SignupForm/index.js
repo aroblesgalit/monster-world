fa48413ef6389858f75d4ec2160b2a305053c793
Alvin Galit <aroblesgalit@gmail.com>
2020-06-23 11:16:36 -0500

    Added h1 headings in login and signup pages

--- client/src/components/SignupForm/index.js
+++ client/src/components/SignupForm/index.js
@@ -54,12 +54,12 @@ function SignupForm() {
                     ref={usernameRef}
                 />
             </div>
-            <div className="mb-6">
+            <div className="mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                     Password
                 </label>
                 <input
-                    className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="password"
                     type="password"
                     placeholder="******************"
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,6 +4,7 @@ import SignupForm from "../components/SignupForm";
 function Signup() {
     return (
         <div className="w-full h-full min-h-screen bg-gray-200 p-8">
+            <h1 className="text-5xl text-center mb-8">Signup</h1>
             <SignupForm />
         </div>
     );
