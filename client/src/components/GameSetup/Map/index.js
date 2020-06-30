15426d3f9dbdd396390158b5643c9d68590062d2
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-06-30 13:32:30 -0500

    Removed unused resources. Added code for possible future use (camera movement) formatted code

--- client/src/components/GameSetup/Map/index.js
+++ client/src/components/GameSetup/Map/index.js
@@ -9,36 +9,37 @@ Getting Started with Scene Breakdown - https://www.youtube.com/watch?v=7cpZ5Y7TH
 
 import Phaser from 'phaser';
 
-class ShowMap extends Phaser.Scene{
-  constructor(game){
-    super({key:"ShowMap"});
-    var game=game;
+class ShowMap extends Phaser.Scene {
+  constructor(game) {
+    super({ key: "ShowMap" });
+    var game = game;
   }
 
-  preload(){ 
+  preload() {
     this.load.image('map', 'Assets/map.png');
   }
 
   create() {
     var gameWidth = this.game.config.width
     var gameHeight = this.game.config.height
+
     // Set background color (on camera in scene)
-    this.cameras.main.setBackgroundColor('#D8DFEF0')
+    this.cameras.main.setBackgroundColor('#D8DFEF')
 
     // Set up map image
-    var map = this.add.image(gameWidth/2, gameHeight/2, 'map');
-    
+    var map = this.add.image(gameWidth / 2, gameHeight / 2, 'map');
+
     // blow up image to 4 times the original size
     // map.setScale(4);
 
     // set map to fill screen, and overflow on the smaller side
-    if(gameWidth > gameHeight){
-      map.displayWidth=gameWidth;
-      map.displayHeight = map.height*map._scaleX;
+    if (gameWidth > gameHeight) {
+      map.displayWidth = gameWidth;
+      map.displayHeight = map.height * map._scaleX;
     }
-    else{
-      map.displayHeight=gameHeight;
-      map.displayWidth = map.width*map._scaleY;
+    else {
+      map.displayHeight = gameHeight;
+      map.displayWidth = map.width * map._scaleY;
     }
 
     // set interactive and dragable
@@ -49,16 +50,36 @@ class ShowMap extends Phaser.Scene{
     map.on('drag', function (pointer, dragX, dragY) {
       this.y = dragY;
       // Confine movement to map height
-      if (this.y-this.displayHeight/2 > 0){this.y=this.displayHeight/2}
-      if (this.y+this.displayHeight/2 < gameHeight)(this.y=gameHeight-this.displayHeight/2)
+      if (this.y - this.displayHeight / 2 > 0) { this.y = this.displayHeight / 2 }
+      if (this.y + this.displayHeight / 2 < gameHeight) (this.y = gameHeight - this.displayHeight / 2)
 
       this.x = dragX;
       // Confine movement to map width
-      if (this.x-this.displayWidth/2 > 0){this.x=this.displayWidth/2}
-      if (this.x+this.displayWidth/2 < gameWidth)(this.x=gameWidth-this.displayWidth/2)
-  });
+      if (this.x - this.displayWidth / 2 > 0) { this.x = this.displayWidth / 2 }
+      if (this.x + this.displayWidth / 2 < gameWidth) (this.x = gameWidth - this.displayWidth / 2)
+    });
 
   }
 }
 
 export default ShowMap;
+
+
+
+
+
+// // code for moving the CAMERA, instead of the map itself
+// // ===========================================================
+
+// if (this.game.input.activePointer.isDown) {
+//   if (this.game.origDragPoint) {
+// 	// move the camera by the amount the mouse has moved since last update
+// 	this.cameras.main.scrollX +=
+// 	  this.game.origDragPoint.x - this.game.input.activePointer.position.x;
+// 	this.cameras.main.scrollY +=
+// 	  this.game.origDragPoint.y - this.game.input.activePointer.position.y;
+//   } // set new drag origin to current position
+//   this.game.origDragPoint = this.game.input.activePointer.position.clone();
+// } else {
+//   this.game.origDragPoint = null;
+// }
