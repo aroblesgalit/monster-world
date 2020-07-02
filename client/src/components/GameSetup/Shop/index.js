12506931f17e7a6e3de5a0e1d5a36d5192fa7dc4
Ryan Gautier <ryangautier2@gmail.com>
2020-07-02 13:14:41 -0500

    add sign images and position

--- client/src/components/GameSetup/Shop/index.js
+++ client/src/components/GameSetup/Shop/index.js
@@ -7,10 +7,22 @@ class Shop extends Phaser.Scene{
 
   preload(){ 
     this.load.image('shop', 'assets/shop.jpg');
+    this.load.image('20sign', 'assets/shop20sign.jpg');
+    this.load.image('50sign', 'assets/shop50sign.jpg');
+    this.load.image('freesign', 'assets/shopfreesign.jpg');
+    this.load.image('1Msign', 'assets/shop1Msign.jpg');
   }
 
   create() {
-    this.add.image(0, 0, 'shop').setOrigin(0);
+    this.add.image(0, 0, 'shop').setOrigin(0).setDepth(0);
+    let sign20L = this.add.image(280, 100, '20sign').setOrigin(0).setDepth(1);
+    let sign20R = this.add.image(1480, 100, '20sign').setOrigin(0).setDepth(1);
+
+    let sign50 = this.add.image(1080, 0, '50sign').setOrigin(0).setDepth(1);
+    let freesignL = this.add.image(580, 330, 'freesign').setOrigin(0).setDepth(1);
+    let freesignR = this.add.image(690, 240, 'freesign').setOrigin(0).setDepth(1);
+    let sign1M = this.add.image(1110, 310, '1Msign').setOrigin(0).setDepth(1);
+
     this.cameras.main.setBounds(0, 0, 1920, 1600);
     this.cameras.main.setZoom(0.75);
 
