80d6fb94a258087a24d76aacc49f98223f07b84a
Ryan Gautier <ryangautier2@gmail.com>
2020-07-05 17:04:45 -0500

    change tint of sign on click

--- client/src/components/GameSetup/Shop/index.js
+++ client/src/components/GameSetup/Shop/index.js
@@ -1,11 +1,13 @@
+// guide for making a menu in phaser https://www.youtube.com/watch?v=OS7neDUUhPE
+
 import Phaser from 'phaser';
 let controls;
-class Shop extends Phaser.Scene{
-  constructor(){
-    super({key:"Shop"});
+class Shop extends Phaser.Scene {
+  constructor() {
+    super({ key: "Shop" });
   }
 
-  preload(){ 
+  preload() {
     this.load.image('shop', 'assets/shop.jpg');
     this.load.image('20sign', 'assets/shop20sign.jpg');
     this.load.image('50sign', 'assets/shop50sign.jpg');
@@ -15,13 +17,56 @@ class Shop extends Phaser.Scene{
 
   create() {
     this.add.image(0, 0, 'shop').setOrigin(0).setDepth(0);
-    let sign20L = this.add.image(280, 100, '20sign').setOrigin(0).setDepth(1);
-    let sign20R = this.add.image(1480, 100, '20sign').setOrigin(0).setDepth(1);
+    let sign20L = this.add.image(280, 100, '20sign').setOrigin(0).setDepth(1).setInteractive();
+    let sign20R = this.add.image(1480, 100, '20sign').setOrigin(0).setDepth(1).setInteractive();
 
-    let sign50 = this.add.image(1080, 0, '50sign').setOrigin(0).setDepth(1);
-    let freesignL = this.add.image(580, 330, 'freesign').setOrigin(0).setDepth(1);
-    let freesignR = this.add.image(690, 240, 'freesign').setOrigin(0).setDepth(1);
-    let sign1M = this.add.image(1110, 310, '1Msign').setOrigin(0).setDepth(1);
+    let sign50 = this.add.image(1080, 0, '50sign').setOrigin(0).setDepth(1).setInteractive();
+    let freesignL = this.add.image(580, 330, 'freesign').setOrigin(0).setDepth(1).setInteractive();
+    let freesignR = this.add.image(690, 240, 'freesign').setOrigin(0).setDepth(1).setInteractive();
+    let sign1M = this.add.image(1110, 310, '1Msign').setOrigin(0).setDepth(1).setInteractive();
+
+    // sign onclick functions
+    sign20L.on('pointerdown', function (pointer) {
+      this.setTint(0xccc3c1);
+    });
+    sign20L.on('pointerup', function (pointer) {
+      this.clearTint();
+    });
+
+    sign20R.on('pointerdown', function (pointer) {
+      this.setTint(0xccc3c1);
+    });
+    sign20R.on('pointerup', function (pointer) {
+      this.clearTint();
+    });
+
+    sign50.on('pointerdown', function (pointer) {
+      this.setTint(0xccc3c1);
+    });
+    sign50.on('pointerup', function (pointer) {
+      this.clearTint();
+    });
+
+    freesignL.on('pointerdown', function (pointer) {
+      this.setTint(0xccc3c1);
+    });
+    freesignL.on('pointerup', function (pointer) {
+      this.clearTint();
+    });
+
+    freesignR.on('pointerdown', function (pointer) {
+      this.setTint(0xccc3c1);
+    });
+    freesignR.on('pointerup', function (pointer) {
+      this.clearTint();
+    });
+
+    sign1M.on('pointerdown', function (pointer) {
+      this.setTint(0xccc3c1);
+    });
+    sign1M.on('pointerup', function (pointer) {
+      this.clearTint();
+    });
 
     this.cameras.main.setBounds(0, 0, 1920, 1600);
     this.cameras.main.setZoom(0.75);
@@ -42,15 +87,15 @@ class Shop extends Phaser.Scene{
     };
     controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
 
-    this.input.on('pointerdown', function(event) {
+    this.input.on('pointerdown', function (event) {
       if (event.x < 356 && event.x > 175 && event.y > 125 && event.y < 537) {
-        console.log("clicked bookcase ", event.x + " : " + event.y );
+        console.log("clicked bookcase ", event.x + " : " + event.y);
       }
 
-    },)
+    })
   }
 
-  update(time,delta) {
+  update(time, delta) {
     controls.update(delta);
   }
 }
