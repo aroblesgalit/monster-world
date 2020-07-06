74db0f9705092aaaa8ce4a51ebe4c3e1df8ad7b7
Alvin Galit <aroblesgalit@gmail.com>
2020-07-06 11:53:03 -0500

    Added button with text for build

--- client/src/components/GameSetup/Farm/index.js
+++ client/src/components/GameSetup/Farm/index.js
@@ -13,15 +13,48 @@ class Farm extends Phaser.Scene {
             frameHeight: 14
         });
         this.load.image("dirt", "Assets/tilledDirt.png");
+        this.load.image("buttonUp", "Assets/blue_button04.png");
+        this.load.image("buttonDown", "Assets/blue_button05.png");
+        this.load.image("buttonHover", "Assets/blue_button02.png");
     }
 
     create() {
 
-        const crop = this.add.sprite(200, 100, "crop", 0);
-        const dirt = this.add.image(100, 100, "dirt", 0).setDepth(1);
-        crop.setDepth(1);
+        // Width and Height of game setup
+        let gameW = this.sys.game.config.width;
+        let gameH = this.sys.game.config.height;
 
-        var config = {
+        // Set up farmBg image to centered on screen
+        // this.image = this.add.image(this.game.config.width / 2, this.game.config.height / 2, "farmBg");
+        let grass = this.add.image(0, 0, "grass")
+            .setOrigin(0, 0)
+            .setScale(0.4);
+        this.cameras.main.setBounds(0, 0, 0, 0);
+        this.cameras.main.setZoom(3);
+
+        // Camera controls
+        let cursors = this.input.keyboard.createCursorKeys();
+
+        let controlConfig = {
+            camera: this.cameras.main,
+            left: cursors.left,
+            right: cursors.right,
+            up: cursors.up,
+            down: cursors.down,
+            acceleration: 0.06,
+            drag: 0.0005,
+            maxSpeed: 1.0
+        };
+
+        controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
+
+        const dirt = this.add.image(100, 100, "dirt", 0)
+
+        // Crop
+        const crop = this.add.sprite(200, 100, "crop")
+        console.log(crop);
+
+        let config = {
             key: "cropAnimation",
             frames: this.anims.generateFrameNumbers("crop", {
                 start: 5, end: 0
@@ -39,37 +72,34 @@ class Farm extends Phaser.Scene {
             console.log(pointer);
         });
 
-        // Button
-        var button = this.add.text(100, 100, "Build", { fill: "#fff" }).setDepth(1);
-        button.setInteractive();
+        // Build Button
+        let buildBtn = this.add.image(0, 0, "buttonUp")
+            .setOrigin(0)
+            .setScale(0.3)
+            .setInteractive({ useHandCursor: true })
+            .on("pointerdown", () => {
+                buildBtn.setTexture("buttonDown")
+                    .setPosition(0, 1);
+                buildBtnText.setPosition(buildBtn.x + 18, buildBtn.y);
+            })
+            .on("pointerover", () => {
+                buildBtn.setTexture("buttonHover");
+            })
+            .on("pointerout", () => {
+                buildBtn.setTexture("buttonUp")
+                    .setPosition(0, 0);
+                buildBtnText.setPosition(buildBtn.x + 18, buildBtn.y);
 
-        button.on("pointerdown", () => {
-            console.log("Button clicked!")
-        })
+            })
+        let buildBtnText = this.add.text(buildBtn.x + 18, buildBtn.y, "Build", { font: "10px Arial", fill: "#000" });
+        this.add.container(100, this.cameras.main.height / 3.3, [buildBtn, buildBtnText]);
+        // const buildBtn = this.add.text(100, 100, "Build", { fill: "#fff" })
+        //     .setDepth(1)
+        //     .setInteractive({ useHandCursor: true })
+        //     .on("pointerdown", () => {
+        //     console.log("Button clicked!")
+        // })
 
-
-        // Set up farmBg image to centered on screen
-        // this.image = this.add.image(this.game.config.width / 2, this.game.config.height / 2, "farmBg");
-        var grass = this.add.image(0, 0, "grass").setOrigin(0);
-        grass.setScale(0.9);
-        this.cameras.main.setBounds(0, 0, 0, 0);
-        this.cameras.main.setZoom(3);
-
-        // Camera controls
-        var cursors = this.input.keyboard.createCursorKeys();
-
-        var controlConfig = {
-            camera: this.cameras.main,
-            left: cursors.left,
-            right: cursors.right,
-            up: cursors.up,
-            down: cursors.down,
-            acceleration: 0.06,
-            drag: 0.0005,
-            maxSpeed: 1.0
-        };
-
-        controls = new Phaser.Cameras.Controls.SmoothedKeyControl(controlConfig);
     }
 
     update(time, delta) {
