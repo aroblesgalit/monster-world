7882a888357e219618613e2eeeaab7388295da24
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-07-07 02:41:14 -0500

    Changed map to use camera to move around set scene camera to 70% of screen, and offset by 15% of screen

--- client/src/components/GameSetup/Map/index.js
+++ client/src/components/GameSetup/Map/index.js
@@ -13,6 +13,7 @@ class ShowMap extends Phaser.Scene {
   constructor(game) {
     super({ key: "ShowMap" });
     var game = game;
+    var text;
   }
 
   preload() {
@@ -20,19 +21,17 @@ class ShowMap extends Phaser.Scene {
   }
 
   create() {
+    var cam = this.cameras.main;
     var gameWidth = this.game.config.width
     var gameHeight = this.game.config.height
 
-    // Set background color (on camera in scene)
-    this.cameras.main.setBackgroundColor('#D8DFEF')
-
     // Set up map image
-    var map = this.add.image(gameWidth / 2, gameHeight / 2, 'map');
+    var map = this.add.image(0, 0, 'map').setOrigin(0);
 
     // blow up image to 4 times the original size
     // map.setScale(4);
 
-    // set map to fill screen, and overflow on the smaller side
+    // Set map to fill screen, and overflow on the smaller side
     if (gameWidth > gameHeight) {
       map.displayWidth = gameWidth;
       map.displayHeight = map.height * map._scaleX;
@@ -42,22 +41,28 @@ class ShowMap extends Phaser.Scene {
       map.displayWidth = map.width * map._scaleY;
     }
 
-    // set interactive and dragable
-    map.setInteractive(new Phaser.Geom.Rectangle(0, 0, map.width, map.height), Phaser.Geom.Rectangle.Contains);
-    this.input.setDraggable(map);
+    // Set up camera settings on scene
+    cam.setBackgroundColor('#D8DFEF')
+    cam.setBounds(0, 0, map.displayWidth, map.displayHeight);
+    this.cameras.main.setViewport(gameWidth*.15, gameHeight*.15, gameWidth*.7, gameHeight*.7);
+  }
 
-    // set draging functionality
-    map.on('drag', function (pointer, dragX, dragY) {
-      this.y = dragY;
-      // Confine movement to map height
-      if (this.y - this.displayHeight / 2 > 0) { this.y = this.displayHeight / 2 }
-      if (this.y + this.displayHeight / 2 < gameHeight) (this.y = gameHeight - this.displayHeight / 2)
+  update(){
+    var pointer = this.input.activePointer;
+    var cam = this.cameras.main;
 
-      this.x = dragX;
-      // Confine movement to map width
-      if (this.x - this.displayWidth / 2 > 0) { this.x = this.displayWidth / 2 }
-      if (this.x + this.displayWidth / 2 < gameWidth) (this.x = gameWidth - this.displayWidth / 2)
-    });
+    if (pointer.isDown) {
+      if (this.game.origDragPoint) {
+        // move the camera by the amount the mouse has moved since last update
+        this.cameras.main.scrollX +=
+          this.game.origDragPoint.x - this.game.input.activePointer.position.x;
+        this.cameras.main.scrollY +=
+          this.game.origDragPoint.y - this.game.input.activePointer.position.y;
+      } // set new drag origin to current position
+      this.game.origDragPoint = this.game.input.activePointer.position.clone();
+    } else {
+      this.game.origDragPoint = null;
+    }
 
   }
 }
