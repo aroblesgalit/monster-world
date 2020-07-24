7d5832c8b8cebb393f58e7534f170dbd9f95c312
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-07-24 13:48:13 -0500

    Added additional plants to new build menu

--- client/src/components/GameSetup/FarmHUD/index.js
+++ client/src/components/GameSetup/FarmHUD/index.js
@@ -41,6 +41,7 @@ class FarmHUD extends Phaser.Scene {
         this.load.image("buildWindow", "Assets/build_window.png");
         this.load.image("dirt2", "Assets/dirt2.png");
         this.load.image("seeds", "Assets/seeds.png");
+        this.load.spritesheet("plantButtons", "Assets/tilesets/plants.png", {frameWidth:32, frameHeight:64});
     }
 
     create() {
@@ -100,8 +101,11 @@ class FarmHUD extends Phaser.Scene {
         // =================================
         let buildWindow = this.add.image(0, 0, "buildWindow");
         let buildObjects = []
-        buildObjects.push(this.add.image(0, 20, "dirt2").setInteractive({ useHandCursor: true }));
+
+        //buildObjects.push(this.add.image(0, 20, "buttonUp").setInteractive({ useHandCursor: true }));
         buildObjects.push(this.add.image(32, 20, "seeds").setInteractive({ useHandCursor: true }));
+        buildObjects.push(this.add.sprite(-32, 0, "plantButtons").setFrame(46).setInteractive({ useHandCursor: true }));
+
         this.buildContainer = this.add.container(this.cameras.main.width / 2, this.cameras.main.height - 200, [buildWindow, ...buildObjects]).setScale(3).setDepth(2);
         this.buildContainer.setInteractive(new Phaser.Geom.Rectangle(0, 0, buildWindow.width, buildWindow.height), Phaser.Geom.Rectangle.Contains);
         this.buildContainer.visible = false;
@@ -158,6 +162,9 @@ class FarmHUD extends Phaser.Scene {
                     break;
                 case "seeds":
                     className = Carrot;
+                    break;
+                case "plantButtons":
+                    className = Potato;
                     break;
             }
 
